5e4de635a2157873c23f966bede594c3ea4fa6a8
Dane Zeke Liergaard <dzliergaard@gmail.com>
2016-07-12 08:54:23 -0700

    Return RPToolsWebAppInitializer java spring config file

---
+++ src/main/java/com/rptools/spring/RPToolsWebAppInitializer.java
@@ -0,0 +1,53 @@
+/*
+ * RPToolkit - Tools to assist Role-Playing Game masters and players
+ * Copyright (C) 2016 Dane Zeke Liergaard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.rptools.spring;
+
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * Spring web application configuration
+ */
+public class RPToolsWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[]{"/"};
+  }
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class<?>[]{SpringConfiguration.class};
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return null;
+  }
+
+  @Override
+  public void onStartup(ServletContext container) throws ServletException {
+    super.onStartup(container);
+    // Add request/session scope listener
+    container.addListener(RequestContextListener.class);
+  }
+}

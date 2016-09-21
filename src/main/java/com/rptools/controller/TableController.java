af58abd0b31fd5fdde7b5e5347bcde7997f5ab32
Dane Zeke Liergaard <dzliergaard@gmail.com>
2016-09-20 21:50:47 -0700

    Add DMG tables, small changes to party page/card.

--- src/main/java/com/rptools/controller/TableController.java
+++ src/main/java/com/rptools/controller/TableController.java
@@ -1,20 +1,36 @@
 package com.rptools.controller;
 
-import java.util.List;
-import java.util.Map;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Returns the list of table files available at /data/tables/
  */
 @RestController("TableController")
 @RequestMapping("tables")
+@CommonsLog
 public class TableController {
+  private ResourcePatternResolver resolver;
+
+  public TableController() {
+    resolver = new PathMatchingResourcePatternResolver(this.getClass().getClassLoader());
+  }
 
   @RequestMapping(method = RequestMethod.GET)
-  public Map<String, List<String>> getTableURLs() {
-    return null;
+  public Map<String, List<String>> getTableURLs() throws IOException {
+    List<Resource> resources = Lists.newArrayList(resolver.getResources("**/*.txt"));
+    return ImmutableMap.of("data", resources.stream().map(Resource::getFilename).collect(Collectors.toList()));
   }
 }
